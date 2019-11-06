fdcfd6a9913e3edde414a9bc7fe927ad017e488f
bbourdin <bbourdin@murex.com>
2019-11-06 16:48:59 +0100

    Add fullscreen window with blinking colors

--- LunchAlertor.py
+++ LunchAlertor.py
@@ -6,9 +6,7 @@ import sys
 import datetime
 import ctypes  # An included library with Python install.
 import argparse
-
-def popupWarning(title, text, style):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+import tkinter  as tk
 
 parser = argparse.ArgumentParser()
 parser.add_argument("startHour", type=str,
@@ -42,10 +40,35 @@ restrictedItems = appointments.Restrict(restriction)
 # iterate over all meetings
 listDisplayMsg = []
 for appointmentItem in restrictedItems:
-   listDisplayMsg.append("At {0} => {1}".format(appointmentItem.start, appointmentItem.Subject))
+   startDateFormatted = appointmentItem.start.strftime("%H:%M")
+   listDisplayMsg.append("[{0}] => {1}".format(startDateFormatted, appointmentItem.Subject))
+
+class Window:
+   def __init__(self, title, text):
+      self.m_window = tk.Tk()
+      self.m_window.configure(background="red")
+      self.m_window.attributes('-fullscreen', True)
+      self.m_label = tk.Label(self.m_window, text=text, font=("Courier bold", 44), width=400, anchor=tk.CENTER, bg='red', fg='white' )
+      self.m_label.pack(fill='x', expand=True)
+
+   def change_color(self):
+       current_color_window = self.m_window.cget("background")
+       current_color_fg_label = self.m_label.cget("foreground")
+       next_color_window = "white" if current_color_window == "red" else "red"
+       next_color_fg_label = "red" if current_color_fg_label == "white" else "white"
+       self.m_window.config(background=next_color_window)
+       self.m_label.config(background=next_color_window)
+       self.m_label.config(foreground=next_color_fg_label)
+       self.m_window.after(1000, self.change_color)
+
+   def run(self):
+      self.m_window.mainloop()
 
 # display the meetings that we have to remind before going to lunch
 if listDisplayMsg:
    title = "You have appointments between {0} and {1}".format(startHour, endHour)
    text = '\n'.join(listDisplayMsg)
-   popupWarning(title, text, 1)
+   text = "Attention tu as des meetings bientôt\n\n" + text
+   window = Window(title, text)
+   window.change_color()
+   window.run()
